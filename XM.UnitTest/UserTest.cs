ed2c826edb45af28111de2b1d7e3ec659cfdda80
JY <jason0824@foxmail.com>
2019-04-25 10:49:42 +0800

    单元测试-DEMO 1、确保每个底层方法是否正常 2、提供方法调用示例

---
+++ XM.UnitTest/UserTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XM.DALFactory;
+
+namespace XM.UnitTest
+{
+    [TestClass]
+    public class UserTest
+    {
+        internal DALCore DALUtility => DALCore.GetInstance();
+
+        [TestMethod]
+        public void UserLogin()
+        {
+            string UserAccount = "";
+            string UserPwd = "";
+            var currentUser = DALUtility.User.UserLogin(UserAccount, UserPwd);
+            Assert.AreEqual(currentUser.UserAccountName, UserAccount);
+        }
+    }
+}
