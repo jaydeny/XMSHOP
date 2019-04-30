168468bfd775c5168cb683e6bbffcbc36e45c932
the-iceberg <2608428799@qq.com>
2019-04-30 10:23:10 +0800

    会员下单-1

--- XM.WebVip/Scripts/vip/vip@AgoodsList.js
+++ XM.WebVip/Scripts/vip/vip@AgoodsList.js
@@ -25,16 +25,21 @@
 
 // 商品模板
 var strGoods = function (obj) {
-    return "<li><div class='goods-item' ><p class='p-img'><a><img /></a></p><p class='p-title'><a><span>名称</span><span class='red'>亮点</span></a></p><p class='p-price'><b>￥99</b></p><div class='p-button'><a class=''>立即下单</a></div></div ></li >";
+    return "<li><div class='goods-item' ><p class='p-img'><a><img /></a></p><p class='p-title'><a><span>" + obj.goods_name + "</span><span class='red'>亮点</span></a></p><p class='p-price'><b>￥" + obj.price + "</b></p><div class='p-button'><a class='' data-id=" + obj.id+" >立即下单</a></div></div ></li >";
 }
 
 // 渲染商品
 var goodsRender = function (list) {
-
-    $.post("//")
-
-    $.each(list, function (i,n) {
-        $(".goods-exhibition>ul").append(strGoods(n));
-    });
+    $.post("/vip/qryagoods", function (data) {
+        if (data.total > 0) {
+            $.each(data.rows, function (i, n) {
+                $(".goods-exhibition>ul").append(strGoods(n));
+            });
+        }
+    },"json")
 }
+$(".goods-exhibition").on("error", "img", function () {
+    $(this).unbind('error'); //防止替换图片加载失败后陷入无限循环
+    $(this).attr('src', '~/img/failure.png');
+})
 goodsRender(new Array(10));
