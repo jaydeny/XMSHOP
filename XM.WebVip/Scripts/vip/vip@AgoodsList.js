78d3791e162c94f2a66fc71a17d89585aae85790
the-iceberg <2608428799@qq.com>
2019-04-30 11:36:49 +0800

    提交会员

--- XM.WebVip/Scripts/vip/vip@AgoodsList.js
+++ XM.WebVip/Scripts/vip/vip@AgoodsList.js
@@ -1,7 +1,4 @@
-﻿$(".goods-exhibition").on("click", ".p-button", function () {
-    alert("立即下单");
-});
-
+﻿
 // 弹出商品框
 //var bounced = function (obj) {
 //    console.log("ok");
@@ -24,22 +21,33 @@
 //});
 
 // 商品模板
-var strGoods = function (obj) {
-    return "<li><div class='goods-item' ><p class='p-img'><a><img /></a></p><p class='p-title'><a><span>" + obj.goods_name + "</span><span class='red'>亮点</span></a></p><p class='p-price'><b>￥" + obj.price + "</b></p><div class='p-button'><a class='' data-id=" + obj.id+" >立即下单</a></div></div ></li >";
+var strGoods = function (i,obj) {
+    return "<li><div class='goods-item' ><p class='p-img'><a><img src='~/img" + obj.goods_pic + "'  /></a></p><p class='p-title'><a><span>" + obj.goods_name + "</span><span class='red'>" + obj.goods_intro + "</span></a></p><p class='p-price'><b>￥" + obj.price + "</b></p><div class='p-button' data-id=" + i +" ><a class='' >立即下单</a></div></div ></li >";
 }
+
+var listGoods
 
 // 渲染商品
 var goodsRender = function (list) {
     $.post("/vip/qryagoods", function (data) {
+        console.log(data);
         if (data.total > 0) {
+            listGoods = data.rows;
             $.each(data.rows, function (i, n) {
-                $(".goods-exhibition>ul").append(strGoods(n));
+                $(".goods-exhibition>ul").append(strGoods(i,n));
             });
         }
     },"json")
 }
-$(".goods-exhibition").on("error", "img", function () {
-    $(this).unbind('error'); //防止替换图片加载失败后陷入无限循环
-    $(this).attr('src', '~/img/failure.png');
-})
+
+$(".goods-exhibition").on("click", ".p-button", function () {
+    var obj = listGoods[$(this).data("id")];
+    $.post("/vip/buy", { goods_id: obj.goods_id, buy_count: 1, order_total: obj.goods_pic, buy_total: obj.goods_pic * 1 }, function (data) {
+        console.log(data);
+    })
+});
+
+// 查询商品类型
+
+// 请求商品
 goodsRender(new Array(10));
