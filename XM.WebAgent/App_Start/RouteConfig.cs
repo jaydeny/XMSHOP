d9c9e2b83f51545ff94303310f43182c382bbe8c
owen <owen@OWEN-DESKTOP>
2019-05-05 11:35:52 +0800

    修改路由规则,由于出现多个homeController,需要添加namespaces

--- XM.WebAgent/App_Start/RouteConfig.cs
+++ XM.WebAgent/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace XM.WebAgent
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional },
+                namespaces: new string[] { "XM.WebAgent.Controllers" }
             );
         }
     }
