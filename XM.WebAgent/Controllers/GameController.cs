19a87ee665dbf56c96314fad349eee714fa1af7d
17875 <17875@>
2019-05-22 15:39:46 +0800

    添加游戏记录报表

--- XM.WebAgent/Controllers/GameController.cs
+++ XM.WebAgent/Controllers/GameController.cs
@@ -1,17 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using XM.Comm;
 using System.Web;
 using System.Web.Mvc;
+using XM.Web.Controllers;
 
 namespace XM.WebAgent.Controllers
 {
-    public class GameController : Controller
+    /// <summary>
+    /// 创建人:梁钧淋
+    /// 创建日期:2019-5-17
+    /// 修改日期:2019-5-17
+    /// 功能: 对游戏端API接入，进行游戏端数据处理，可视化
+    /// </summary>
+    public class GameController : BaseController
     {
         // GET: Game
         public ActionResult Index()
         {
             return View();
         }
+
+        /// <summary>
+        /// 游戏报表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GameForm() {
+            return View();
+        }
+        /// <summary>
+        /// 获取所有游戏
+        /// </summary>
+        /// <returns></returns>
+        public string GetGameOrByAccount() {
+            string[] param = {  };
+            return  ReturnRes(param, "GetGameOrByAccount");
+        }
+
+        public string GetRecordCollect()
+        {
+            string vipName = Request["vipName"] == null ? "" : Request["vipName"];
+            string startTime = Request["startTime"] == null ? "" : Request["startTime"];
+            string endTime = Request["endTime"] == null ? "" : Request["endTime"];
+            string[] param = { vipName,startTime,endTime };
+            return ReturnRes(param, "GetRecordCollect");
+        }
+        /// <summary>
+        /// 获取当前时间段（天）的记录数据
+        /// </summary>
+        /// <returns></returns>
+        public string GetRecordCollectByAgency() {
+
+            string ID = Request["ID"] == null ? "" : Request["ID"];
+            string startTime = Request["startTime"] == null ? "" : Request["startTime"];
+            string endTime = Request["endTime"] == null ? "" : Request["endTime"];
+            string[] param = { Session["agent_AN"].ToString(), ID, startTime,endTime };
+            return ReturnRes(param, "GetRecordCollectByAgency");
+        }
+        /// <summary>
+        /// 获取当天的游戏记录数据
+        /// </summary>
+        /// <returns></returns>
+        public string GetRecord() {
+            string ID = Request["ID"] == null ? "" : Request["ID"];
+            string time = Request["time"] == null ? "" : Request["time"];
+            string page = Request["page"] == null ? "" : Request["page"];
+            string rows = Request["rows"] == null ? "" : Request["rows"];
+            string[] param = { "", ID, time, time,page, Session["agent_AN"].ToString(), rows };
+            return ReturnRes(param, "GetRecord");
+        }
+
     }
 }
