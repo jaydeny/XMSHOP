9415ae50f8d3caffc7fd50dfaf2d6e73e1868915
owen <owen@OWEN-DESKTOP>
2019-05-27 15:16:24 +0800

    修改映射

--- XM.Model/CustomFullEntity.cs
+++ XM.Model/CustomFullEntity.cs
@@ -15,22 +15,22 @@ namespace XM.Model
         /// <summary>
         /// 活动id
         /// </summary>
-        public int ac_id { get; set; }
+        public int Ac_id { get; set; }
         /// <summary>
         /// 活动类型id
         /// </summary>
-        public int ac_type { get; set; }
+        public int Ac_type { get; set; }
         /// <summary>
         /// 溢满
         /// </summary>
-        public string ac_full { get; set; }
+        public string Ac_full { get; set; }
         /// <summary>
         /// 赠送积分
         /// </summary>
-        public string minus { get; set; }
+        public string Minus { get; set; }
         /// <summary>
         /// 次数
         /// </summary>
-        public string times { get; set; }
+        public string Times { get; set; }
     }
 }
