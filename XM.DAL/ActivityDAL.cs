5474a289132452687a00e82deea2ed6a844e5fc1
owen <owen@OWEN-DESKTOP>
2019-06-05 11:06:28 +0800

    1

--- XM.DAL/ActivityDAL.cs
+++ XM.DAL/ActivityDAL.cs
@@ -25,7 +25,7 @@ namespace XM.DAL
         /// <returns></returns>
         public List<ActivityEntity> QryAC<ActivityEntity>(Dictionary<string, object> param)
         {
-            string sql = "select * from tbActivity where ( Receiver is null or Receiver = @agent_AN) and StartDate < @Date and EndDate > @Date";
+            string sql = "select * from tbActivity where (Receiver is null or Receiver ='agent0' or  Receiver like '%agent0%') and StartDate < @Date and EndDate > @Date";
             IEnumerable<ActivityEntity> list = QueryList<ActivityEntity>(sql, param);
             List<ActivityEntity> result = list.ToList();
             return result ;
